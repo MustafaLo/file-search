cf2a5ee69112297315224d9dadc69801b5f5d7a2
MustafaLo <mustafalonandwala04@gmail.com>
2025-03-03 23:10:55 +0100

    recursively walked directories for all file paths

--- cmd/search.go
+++ cmd/search.go
@@ -5,22 +5,27 @@ package cmd
 
 import (
 	"fmt"
-	"os"
+	"io/fs"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
 )
 
-func getDirectoryFiles()(string, error){
-	files, err := os.ReadDir(directory)
+func getDirectoryFiles()([]string, error){
+	var files []string
+
+	err := filepath.WalkDir(directory, func(path string, d fs.DirEntry, err error) (error){
+		if !d.IsDir(){
+			files = append(files, path)
+		}
+		return nil
+	})
+
 	if err != nil{
-		return "", err
+		return nil, err
 	}
 
-	for _, f := range(files){
-		fmt.Println(f.Name())
-	}
-
-	return "", nil
-
+	return files, nil
 }
 
 var search_term string
@@ -30,7 +35,15 @@ var searchCmd = &cobra.Command{
 	Short: "search a keyword",
 	Long:  `Use this command to search for a keyword within your directory`,
 	Run: func(cmd *cobra.Command, args []string) {
-	  getDirectoryFiles()
+	  files, err := getDirectoryFiles()
+	  if err != nil{
+		fmt.Printf("Error retrieving files from directory: %v", err)
+		return
+	  }
+
+	  for _, file_path := range(files){
+		fmt.Println(file_path)
+	  }
 	},
 }
 
